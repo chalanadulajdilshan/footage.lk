768649e1889e1acd63c44024acc94162689326dd
hansajith18 <hansajith18@gmail.com>
2022-06-11 17:19:11 +0530

    index page, separate components header & footer

---
+++ components/header.php
@@ -0,0 +1,47 @@
+  <!-- Preloader -->
+  <div class="preloader-bg"></div>
+  <div id="preloader">
+      <div id="preloader-status">
+          <div class="preloader-position loader"> <span></span> </div>
+      </div>
+  </div>
+  <!-- Progress scroll totop -->
+  <div class="progress-wrap cursor-pointer">
+      <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+          <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+      </svg>
+  </div>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg">
+      <div class="container">
+          <!-- Logo -->
+          <a class="logo" href="index.html"> <img src="img/logo.png" alt=""> </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="icon-bar"><i class="ti-line-double"></i></span> </button>
+          <!-- Navbar links -->
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+                  <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+                  <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+                  <li class="nav-item"><a class="nav-link" href="services.html">Services</a></li>
+                  <li class="nav-item"><a class="nav-link" href="works.html">Works</a></li>
+                  <li class="nav-item dropdown"> <span class="nav-link"> Gallery <i class="ti-angle-down"></i></span>
+                      <ul class="dropdown-menu last">
+                          <li class="dropdown-item"><a href="gallery-01.html">Gallery 01</a></li>
+                          <li class="dropdown-item"><a href="gallery-02.html">Gallery 02</a></li>
+                          <li class="dropdown-item"><a href="video-gallery.html">Gallery 03</a></li>
+                          <!--
+                            <li class="dropdown-item"><span>Dropdown 2 <i class="ti-angle-right"></i></span>
+                                <ul class="sub-menu">
+                                    <li class="dropdown-item"><a href="#">Dropdown 3</a></li>
+                                    <li class="dropdown-item"><a href="#">Dropdown 3</a></li>
+                                </ul>
+                            </li>
+                            -->
+                      </ul>
+                  </li>
+                  <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+                  <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+              </ul>
+          </div>
+      </div>
+  </nav>
